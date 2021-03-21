22f8644adb50e010352bb6d3d9bdf54d0ab2344b
Georgey <geobro2310@gmail.com>
2021-03-21 22:14:16 +0530

    Setup ChakraUI, HEading, Icon

---
+++ src/components/Addtodolist.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Addtodolist() {
+  return <div>Add</div>;
+}
+
+export default Addtodolist;
---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Heading, VStack, IconButton } from "@chakra-ui/react";
+import { FaSun, FaMoon } from "react-icons/fa";
+import Todolist from "./Todolist";
+import Addtodolist from "./Addtodolist";
+
+function App() {
+  return (
+    <VStack p={4}>
+      <IconButton
+        icon={<FaSun />}
+        isRound="true"
+        size="lg"
+        alignSelf="flex-end"
+      />
+      <Heading
+        mb="8"
+        fontWeight="extrabold"
+        size="2xl"
+        bgGradient="linear(to-r, pink.500, pink.300, blue.500)"
+        bgClip="text"
+      >
+        ToDo Application
+      </Heading>
+      <Todolist />
+      <Addtodolist />
+    </VStack>
+  );
+}
+
+export default App;
