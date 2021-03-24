89e4048566cbb5f74b0daceda410de8830ce0ad7
Georgey <geobro2310@gmail.com>
2021-03-24 23:08:55 +0530

    Add input

--- src/components/Addtodolist.jsx
+++ src/components/Addtodolist.jsx
@@ -1,7 +1,19 @@
+import { HStack, Button, Input } from '@chakra-ui/react'
 import React from 'react'
 
 function Addtodolist() {
-  return <div>Add</div>
+  function handleSubmit(event) {}
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <HStack mt="8">
+        <Input variant="filled" placeholder="Enter todo item" />
+        <Button colorScheme="pink" px="8" type="submit">
+          Add ToDo
+        </Button>
+      </HStack>
+    </form>
+  )
 }
 
 export default Addtodolist
